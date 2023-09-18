6905a0f89a110e16cb7fe15f37172c29d9be7811
kagankuscu <kuscukagan@gmail.com>
2023-09-18 20:30:40 +0300

    App Created.

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kagan Kuscu on 18.09.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
